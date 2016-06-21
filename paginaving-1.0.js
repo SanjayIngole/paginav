357380be16b9fe4d10c54137f7afaf19b1c6f64e
Sanjay Ingole <sanjayuit@gmail.com>
2016-06-21 14:56:59 -0500

    Create paginaving-1.0.js
    First Draft. To be updated.

---
+++ paginaving-1.0.js
@@ -0,0 +1,20 @@
+var totalItems = 0;
+var itemsPerLine = 10;
+var from = 0;
+var to = 0;
+
+function navFirst(){
+
+}
+
+function navLast(){
+
+}
+
+function navPrevious(count){
+
+}
+
+function navNext(count){
+
+}
